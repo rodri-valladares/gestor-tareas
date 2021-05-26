9cb0cc361b3b27d4dc006a3878ea232573fcc688
rodriV <utndavid@gmail.com>
2021-05-26 00:25:27 -0300

    Crea formulario BASE para crear tareas, guardarlas y mostrarlas en lista

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'web'
 urlpatterns = [
-    path('', views.index, name='index'), #responde a la funcion index
+    path('', views.index, name='index'), #responde a la funcion index de view.py
+    path("nueva_tarea/", views.nueva_tarea, name="nueva_tarea") #response a la funcion nueva_tarea de view.py
 ]
--- web/views.py
+++ web/views.py
@@ -3,3 +3,16 @@ from .models import Tarea
 
 def index(request):
     return render(request, "web/index.html")
+
+def nueva_tarea(request):
+    nombre = request.GET["nombre"]
+    
+    Tarea(
+        nombre=nombre,
+    ).save()
+
+    context = {"tareas": Tarea.objects.all()}
+
+    # print(fecha_de_nacimiento)
+
+    return render(request, "web/index.html", context=context)
